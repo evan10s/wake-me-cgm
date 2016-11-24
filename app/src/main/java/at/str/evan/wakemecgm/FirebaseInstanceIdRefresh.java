0cca11f3be4f082381066175076d600d58e0e8c4
Evan Strat <evan10s@users.noreply.github.com>
2016-11-24 16:34:49 -0500

    Show notification when FCM received

---
+++ app/src/main/java/at/str/evan/wakemecgm/FirebaseInstanceIdRefresh.java
@@ -0,0 +1,22 @@
+package at.str.evan.wakemecgm;
+
+import android.util.Log;
+
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.FirebaseInstanceIdService;
+
+/**
+ * Created by Evan on 11/24/2016.
+ */
+
+public class FirebaseInstanceIdRefresh extends FirebaseInstanceIdService {
+    @Override
+    public void onTokenRefresh() {
+        // Get updated InstanceID token.
+        String refreshedToken = FirebaseInstanceId.getInstance().getToken();
+        Log.d("WAKEMECGM", "Refreshed token: " + refreshedToken);
+
+        // TODO: Implement this method to send any registration to your app's servers.
+        //sendRegistrationToServer(refreshedToken);
+    }
+}
