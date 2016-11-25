0b96c2466f10ed23e45d314060a72f8cd466211a
Evan Strat <evan10s@users.noreply.github.com>
2016-11-25 12:23:46 -0500

    Optimized imports, split out notifications for low, normal, and high BGs
    to provide different vibration patterns for different alerts

--- app/src/main/java/at/str/evan/wakemecgm/bgUpdateService.java
+++ app/src/main/java/at/str/evan/wakemecgm/bgUpdateService.java
@@ -3,16 +3,11 @@ package at.str.evan.wakemecgm;
 import android.app.Notification;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
-import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.Color;
-import android.media.AudioAttributes;
 import android.media.RingtoneManager;
 import android.net.Uri;
-import android.os.IBinder;
-import android.os.Vibrator;
-import android.provider.MediaStore;
 import android.support.v7.app.NotificationCompat;
 import android.util.Log;
 
@@ -34,8 +29,6 @@ public class BgUpdateService extends FirebaseMessagingService {
         //Vibrator vibrate = (Vibrator) getSystemService(VIBRATOR_SERVICE);
         //vibrate.vibrate(300);
         Log.d("WAKEMECGM", remoteMessage.getData().toString());
-
-        int vibrate;
 
         Map<String, String> msgData = remoteMessage.getData();
         int bg = Integer.parseInt(msgData.get("latest_bg"));
@@ -70,57 +63,72 @@ public class BgUpdateService extends FirebaseMessagingService {
         String body;
 
         if (bg < 70) {
-            vibrate = 1;
             body = "Low BG";
-        } else if (bg > 240) {
-            vibrate = 2;
+            notifyLow(title,body);
+        } else if (bg > 130) {
             body = "High BG";
+            notifyHigh(title,body);
         } else {
-            vibrate = 3;
             body = "OK";
+            notifyNorm(title,body);
         }
 
-        sendNotification(title, body,bg);
+
     }
 
-    private void sendNotification(String messageTitle, String messageBody,int bg) {
+    private NotificationCompat.Builder constructNotification(String title, String body) {
         Intent intent = new Intent(this, MainActivity.class);
         intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
         PendingIntent pendingIntent = PendingIntent.getActivity(this, 0 /* request code */, intent, PendingIntent.FLAG_UPDATE_CURRENT);
-        long[] vibrateLow = {400, 400, 400, 400, 400, 400};
         Uri defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
-
-
-        if (bg < 130) {
-            defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_ALARM);
-        }
-
-        NotificationCompat.Builder notificationBuilder = (NotificationCompat.Builder) new NotificationCompat.Builder(getApplicationContext())
+        return (NotificationCompat.Builder) new NotificationCompat.Builder(getApplicationContext())
                 .setSmallIcon(R.mipmap.ic_launcher)
-                .setContentTitle(messageTitle)
-                .setContentText(messageBody)
-                .setVibrate(vibrateLow)
+                .setContentTitle(title)
+                .setContentText(body)
                 .setAutoCancel(true)
                 .setLights(Color.BLUE, 1, 1)
                 .setSound(defaultSoundUri)
-                .setContentIntent(pendingIntent)
-                .setColor(Color.YELLOW);
+                .setContentIntent(pendingIntent);
+    }
 
+    private void notifyLow(String messageTitle, String messageBody) {
+        long[] vibrateLow = {400, 400, 400, 400, 400, 400};
+
+       /* if (bg < 130) {
+            defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_ALARM);
+        }*/
+
+        NotificationCompat.Builder notificationBuilder = constructNotification(messageTitle,messageBody);
+        notificationBuilder.setVibrate(vibrateLow)
+                .setColor(Color.RED);
         Notification bgAlert = notificationBuilder.build();
         bgAlert.flags = Notification.FLAG_INSISTENT;
 
-/*        long[] vibrateLow = {400, 400, 400, 400, 400, 400};
-        long[] vibrateHigh = {500, 400, 500, 400};
-        long[] vibrateOther = {1000};
-        long[] vibrate;
-        if (pattern == 1) {
-            notificationBuilder.setVibrate(vibrateLow);
-        } else if (pattern == 2) {
-            notificationBuilder.setVibrate(vibrateHigh);
-        } else {
-            notificationBuilder
-        }*/
+        sendNotification(notificationBuilder);
+    }
 
+    private void notifyNorm(String messageTitle, String messageBody) {
+        long[] vibrateNorm = {};
+
+        NotificationCompat.Builder notificationBuilder = constructNotification(messageTitle,messageBody);
+        notificationBuilder.setVibrate(vibrateNorm);
+        //Notification bgAlert = notificationBuilder.build();
+
+        sendNotification(notificationBuilder);
+    }
+
+    private void notifyHigh(String messageTitle, String messageBody) {
+        long[] vibrateHigh = {500,500,500,500};
+
+        NotificationCompat.Builder notificationBuilder = constructNotification(messageTitle,messageBody);
+        notificationBuilder.setVibrate(vibrateHigh)
+                .setColor(Color.YELLOW);
+        //Notification bgAlert = notificationBuilder.build();
+
+        sendNotification(notificationBuilder);
+    }
+
+    private void sendNotification(NotificationCompat.Builder notificationBuilder) {
         NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
         notificationManager.notify(0 /* ID of notification */, notificationBuilder.build());
     }
